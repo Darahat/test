27906fa94f0aa7d5aae0cd401e23552d0c9178dc
Didarul Alam Rahat <darahat42@gmail.com>
2024-03-30 23:58:28 +0600

    routing problem solved

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,10 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
+import Navbar from './layouts/default/AppBar.vue';
 
 const app = createApp(App)
+app.component('navbar', Navbar);
 
 registerPlugins(app)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,8 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
+import { createRouter, createWebHistory } from 'vue-router'
+// import { setupLayouts } from 'virtual:generated-layouts'
 import Home from '@/pages/home.vue'
 import About from '@/pages/about.vue'
 import Index from '@/pages/index.vue'
@@ -14,7 +14,7 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: [{ path: '/home', component: Home }, { path: '/about', component: About }, { path: '/', component: Index }],
 
-  extendRoutes: setupLayouts,
+  // extendRoutes: setupLayouts,
 })
 
 export default router
