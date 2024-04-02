2378e1985d7aff611e95076097d55137cf51fbf7
Didarul Alam Rahat <darahat42@gmail.com>
2024-04-02 20:22:46 +0600

    router added dynamic

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,15 +5,18 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+// import { createRouter, createWebHistory } from 'vue-router'
 // import { setupLayouts } from 'virtual:generated-layouts'
-import Home from '@/pages/home.vue'
-import About from '@/pages/about.vue'
-import Index from '@/pages/index.vue'
+// import Home from '@/pages/home.vue'
+// import About from '@/pages/about.vue'
+// import Index from '@/pages/index.vue'
 const router = createRouter({
+  // history: createWebHistory(process.env.BASE_URL),
+  // routes: [{ path: '/home', component: Home }, { path: '/about', component: About }, { path: '/', component: Index }],
   history: createWebHistory(process.env.BASE_URL),
-  routes: [{ path: '/home', component: Home }, { path: '/about', component: About }, { path: '/', component: Index }],
-
+  extendRoutes: setupLayouts,
   // extendRoutes: setupLayouts,
 })
 
